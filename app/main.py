02da82049089bab2e482f0da3cd2824a3bb17597
GaneshManal <ganesh.manal@maplelabs.com>
2020-11-01 01:57:32 +0530

    green deployment

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python! I am blue deployment"
+    return "Hello from Python! I am green deployment"
 
 
 if __name__ == "__main__":
